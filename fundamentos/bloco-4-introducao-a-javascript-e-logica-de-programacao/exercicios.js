7bd8ea17f21f5ccfc69632354b46f9a9b535fe0d
marcosmoura141 <marcosmoura141@gmail.com>
2022-04-07 06:28:46 -0300

    exercuçao do primeiro quesito

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios.js
@@ -0,0 +1,21 @@
+//Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados.//
+const a = 5;
+const b = 6;
+console.log( a + b)
+
+const a = 5;
+const b = 6;
+console.log( a - b)
+
+const a = 5;
+const b = 6;
+console.log( a * b)
+
+const a = 5;
+const b = 6;
+console.log( a / b)
+
+const a = 5;
+const b = 6;
+console.log( a % b)
+
